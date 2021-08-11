c055d0089d93ed80c8c2a98b14e811274bf54ad8
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-08-11 00:59:50 -0400

    added picture nd heading to page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+ const [info, setInfo] = useState([]);
+ const div1 = document.createElement('span');
+  //div1.innerHTML = info;
+  console.log(div1);
+
+ useEffect(() => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(res => {
+  setInfo(res.data)
+  console.log(res.data);
+})
+ }, []);
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1><a href={info.url}>{info.title}</a></h1>
+      <h3>{info.copyright}</h3>
+      <h5>{info.date}</h5>
+      <p>{info.explanation}</p>
+      
+      <img src={info.hdurl} ></img>
     </div>
   );
 }
